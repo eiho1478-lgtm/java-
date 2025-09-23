9b2531d34471800a128814a22c51a3b1a1cd2d16
eiho1 <eiho1@MSI>
2025-09-24 00:19:56 +0900

    java導入

---
+++ HelloWorld_eihoSakiho/src/sample/SelfIntroduction.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "永穂 咲帆";
+		int age = 21;
+		String hobby = "ゲーム";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+//		String name = "永穂 咲帆";
+//		int age = 21;
+//		String hobby = "ゲーム";
+		
+		/*
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		*/
+	}
+
+}
