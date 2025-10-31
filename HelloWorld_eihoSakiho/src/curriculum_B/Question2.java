34145294567bd1c989fd5323aa9baf178d4b2963
eiho1 <eiho1@MSI>
2025-10-31 13:52:54 +0900

    Java基礎問題_2

---
+++ HelloWorld_eihoSakiho/src/curriculum_B/Question2.java
@@ -0,0 +1,117 @@
+package curriculum_B;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+	int score = 75;
+	if (score >= 60) { 
+	 System.out.println("合格です！");}
+	
+	int age = 25;
+	if (age >= 20) {
+	 System.out.println("適正年齢です");
+	} else if (age <= 30) {
+	} else {
+	 System.out.println("対象外です");
+	}
+	
+	int age1 = 18;
+	if (age1 >= 20) {
+	 System.out.println("成人です");
+	} else if (age1 >= 13 && age1 <= 19) {
+	 System.out.println("ティーンエイジャーです");
+	} else {
+	 System.out.println("子供です");
+	}
+	
+	int x = 30;
+	int y = 15;
+	int z = 50;
+	if (x >= y && x >= z) {
+	 System.out.println("一番大きいのは x です：" + x);
+	} else if (y >= x && y >= z) {
+	 System.out.println("一番大きいのは y です：" + y);
+	} else {
+	 System.out.println("一番大きいのは z です：" + z);
+	}
+	
+	int num = -7;
+	if (num > 0) {
+	 System.out.println("正の数です");
+	} else if (num == 0) {
+	 System.out.println("0 です");
+	} else {
+	 System.out.println("負の数です");
+	}
+	
+	int value = 3;
+	if (value % 2 == 0) {
+	 System.out.println("偶数です");
+	} else {
+	 System.out.println("奇数です");
+	}
+	
+	int Score =77;
+	if (Score >= 90) {
+	 System.out.println("優");
+	} else if (Score >= 70) {
+	 System.out.println("良");
+	} else if (Score >= 50) {
+	 System.out.println("可");
+	} else {
+	 System.out.println("不可");
+	}
+	
+	String name = null;
+	if (name == null) {
+	 System.out.println("入力が無効です");
+	}
+	
+	int day = 4;
+	switch (day) {
+	 case 1:
+	  System.out.println("月曜日");
+	  break;
+	 case 2:
+	  System.out.println("火曜日");
+	  break;
+	 case 3:
+	  System.out.println("水曜日");
+	  break;
+	 case 4:
+	 System.out.println("木曜日");
+	  break;
+	 case 5:
+	  System.out.println("金曜日");
+	  break;
+	 case 6:
+	  System.out.println("土曜日");
+	  break;
+	 case 7:
+	  System.out.println("日曜日");
+	  break;
+	 default:
+	  System.out.println("無効な入力です");
+	}
+	
+	int month = 8;
+	switch (month) {
+	 case 12: case 1: case 2:
+	  System.out.println("冬");
+	  break;
+	 case 3: case 4: case 5:
+	  System.out.println("春");
+	  break;
+	 case 6: case 7: case 8:
+	  System.out.println("夏");
+	  break;
+	 case 9: case 10: case 11:
+	  System.out.println("秋");
+	  break;
+	 default:
+	  System.out.println("無効な月です");
+	}
+
+	}
+	}
